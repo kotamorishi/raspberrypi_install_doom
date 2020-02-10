1188ef1f0c29d0201e671e44baf8b68fe256f62b
Kota <44553544+kotamorishi@users.noreply.github.com>
2020-02-09 22:25:33 -0500

    fix the folder name

--- install_doom.sh
+++ install_doom.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt-get install -y libsdl-mixer1.2-dev libsdl-net1.2-dev python-pil
 wget https://www.chocolate-doom.org/downloads/3.0.0/chocolate-doom-3.0.0.tar.gz
 tar xzf chocolate-doom-3.0.0.tar.gz
-cd chocolate-doom-3.0.0.tar.gz
+cd chocolate-doom-3.0.0
 wget http://www.doomworld.com/3ddownloads/ports/shareware_doom_iwad.zip -O
 ./configure
 make
