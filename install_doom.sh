86295465e9195e0b0579adc20e5ce66f4bbdb027
Kota <44553544+kotamorishi@users.noreply.github.com>
2020-02-09 22:27:26 -0500

    Update install_doom.sh

--- install_doom.sh
+++ install_doom.sh
@@ -4,7 +4,8 @@ sudo apt-get install -y libsdl-mixer1.2-dev libsdl-net1.2-dev python-pil
 wget https://www.chocolate-doom.org/downloads/3.0.0/chocolate-doom-3.0.0.tar.gz
 tar xzf chocolate-doom-3.0.0.tar.gz
 cd chocolate-doom-3.0.0
-wget http://www.doomworld.com/3ddownloads/ports/shareware_doom_iwad.zip -O
+wget http://www.doomworld.com/3ddownloads/ports/shareware_doom_iwad.zip -O shareware_doom_iwad.zip
+unzip shareware_doom_iwad.zip
 ./configure
 make
 sudo make install
