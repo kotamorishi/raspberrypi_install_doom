a81205d5eeab010be385e6feb1b10ea36c6e3c81
Kota <44553544+kotamorishi@users.noreply.github.com>
2020-02-09 22:34:08 -0500

    updated names.

--- install_doom.sh
+++ install_doom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-sudo apt-get install -y libsdl-mixer1.2-dev libsdl-net1.2-dev python-pil
+sudo apt-get install -y libsdl2-dev libsdl2-mixer-dev libsdl2-net-dev python-pil
 wget https://www.chocolate-doom.org/downloads/3.0.0/chocolate-doom-3.0.0.tar.gz
 tar xzf chocolate-doom-3.0.0.tar.gz
 cd chocolate-doom-3.0.0
